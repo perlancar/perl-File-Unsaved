4460ac62d1e66d276e20f0f4edd88b5e4efa8e27
perlancar (@pc-office) <perlancar@gmail.com>
2014-12-24 14:43:30 +0700

    Add options: check_pid & check_proc_name, add more details to result

--- lib/File/Unsaved.pm
+++ lib/File/Unsaved.pm
@@ -22,13 +22,10 @@ This function tries, using some heuristics, to find out if a file is being
 opened and has unsaved modification in an editor. Currently the supported
 editors are: Emacs, joe, vim.
 
-Return false if no unsaved data is detected, or else a hash structure. Hash will
-contain these keys: `editor` (kind of editor).
-
 The heuristics are as folow:
 
-* Emacs and joe: check whether `.#<name>` symlink exists. Emacs targets the
-  symlink to `<user>@<host>.<PID>:<timestamp>` while joe to
+* Emacs, joe, mc: check whether `.#<name>` symlink exists. Emacs targets the
+  symlink to `<user>@<host>.<PID>:<timestamp>` while joe and mc to
   `<user>@<host>.<PID>`. Caveat: Unix only.
 
 * vim: check whether `.<name>.swp` file exists, not older than file, and its
@@ -43,32 +40,96 @@ _
             req => 1,
             pos => 0,
         },
+        check_pid => {
+            summary => 'Whether to check that PID is actually an editor',
+            schema  => 'bool*',
+            default => 1,
+            description => <<'_',
+
+A temporary file might be stale, so checking the existence of temporary file is
+not enough. If the temporary file provides pointer to a PID, and this setting is
+set to true, will actually check that the PID exists.
+
+_
+        },
+        check_proc_name => {
+            summary => 'Whether to check that process name is actually the '.
+                'corresponding editor',
+            schema  => 'bool*',
+            default => 1,
+            description => <<'_',
+
+Is activated only `check_pid` is also 1 and if `Proc::Find` (and thus
+`Proc::ProcessTable`) is available.
+
+Might produce a false negative if you happen to rename the editor or use a
+differently-named fork/derivative of said editor, although this should be rare.
+
+_
+        },
     },
     result_naked => 1,
+    result => {
+        schema => ['any*', of=>['bool*', 'hash*']],
+        description => <<'_',
+
+Return false if no unsaved data is detected, or else a hash structure. Hash will
+contain these keys: `editor` (kind of editor, possible values: `emacs`,
+`joe/mc`, `joe`, `mc`, `vim`) and might contain these keys: `pid` (PID of
+editor), `user`, `host`, `timestamp`.
+
+
+_
+    },
 };
 sub check_unsaved_file{
     require File::Spec;
 
     my %args = @_;
-
+    my $check_pid = $args{check_pid} // 1;
+    my $check_proc_name = $args{check_proc_name} // 1;
     my $path = $args{path};
+
     (-f $path) or die "File does not exist or not a regular file";
 
     my ($vol, $dir, $file) = File::Spec->splitpath($path);
 
-    # emacs & joe
+    # emacs & joe/mc
+  CHECK1:
     {
         my $spath = File::Spec->catpath($vol, $dir, ".#$file");
         last unless -l $spath;
         my $target = readlink $spath;
-        if ($target =~ /:\d+$/) {
-            return {editor=>'emacs'};
-        } else {
-            return {editor=>'joe'};
+        if ($target =~ /\A(.+)\@(.+)\.(\d+):(\d+)\z/) {
+            my $res = {editor=>'emacs',
+                       user=>$1, host=>$2, pid=>$3, timestamp=>$4};
+            if ($check_pid) {
+                last CHECK1 unless kill(0, $res->{pid});
+                if ($check_proc_name && eval {require Proc::Find; 1}) {
+                    last CHECK1 unless Proc::Find::proc_exists(
+                        pid => $res->{pid}, name => qr/\b(emacs)\b/,
+                    );
+                }
+            }
+            return $res;
+        } elsif ($target =~ /\A(.+)\@(.+)\.(\d+)\z/) {
+            my $res = {editor=>'joe/mc',
+                       user=>$1, host=>$2, pid=>$3};
+            if ($check_pid) {
+                last CHECK1 unless kill(0, $res->{pid});
+                if ($check_proc_name && eval {require Proc::Find; 1}) {
+                    my $findres = Proc::Find::proc_find(
+                        pid => $res->{pid}, name => qr/\b(joe|mc)\b/);
+                    last CHECK1 unless $findres;
+                    $res->{editor} = $findres->{name};
+                }
+            }
+            return $res;
         }
     }
 
     # vim
+  CHECK_VIM:
     {
         my $spath = File::Spec->catpath($vol, $dir, ".$file.swp");
         last unless -f $spath;
--- t/01-basic.t
+++ t/01-basic.t
@@ -16,14 +16,19 @@ write_file("$dir/a.txt", "");
 dies_ok { check_unsaved_file(path=>"$dir/foo") } "nonexisting file";
 ok(!check_unsaved_file(path=>"$dir/a.txt"), "unmodified");
 
-subtest "emacs & joe" => sub {
+subtest "emacs & joe/mc" => sub {
     plan skip_all => "symlink() not available" unless eval { symlink "",""; 1 };
     write_file("$dir/b.txt", "");
     symlink 'user@host.1234', "$dir/.#b.txt";
-    is_deeply(check_unsaved_file(path=>"$dir/b.txt"), {editor=>"joe"});
+    is_deeply(check_unsaved_file(path=>"$dir/b.txt", check_pid=>0),
+              {editor=>"joe/mc", user=>"user", host=>"host", pid=>1234});
     write_file("$dir/c.txt", "");
     symlink 'user@host.1234:1409321328', "$dir/.#c.txt";
-    is_deeply(check_unsaved_file(path=>"$dir/c.txt"), {editor=>"emacs"});
+    is_deeply(check_unsaved_file(path=>"$dir/c.txt", check_pid=>0),
+              {editor=>"emacs",
+               user=>"user", host=>"host", pid=>1234, timestamp=>1409321328});
+    # XXX check with check_pid=>1
+    # XXX check with check_proc_name=>1
 };
 
 subtest "vim" => sub {
